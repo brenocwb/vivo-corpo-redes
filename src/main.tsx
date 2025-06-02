2e9b82f478054c20d736fa473d49b171e0665972
brenocwb <escolhasuaarte@gmail.com>
2025-06-02 18:16:28 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from './context/AuthContext';
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
