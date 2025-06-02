d99114ad1abf24d03dcf1b0dbaf847fd377ed3e8
brenocwb <escolhasuaarte@gmail.com>
2025-06-02 18:35:02 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from './context/AuthContext';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from '@/context/AuthContext';
+import LoginPage from '@/pages/LoginPage';
+import './globals.css';
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          {/* Adicione outras rotas aqui */}
+        </Routes>
+      </Router>
+    </AuthProvider>
+  </React.StrictMode>
 );
