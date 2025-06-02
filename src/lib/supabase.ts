c7c18e61cf210bbaf7cf4ad57c9bd19f3e567eb1
brenocwb <escolhasuaarte@gmail.com>
2025-06-02 18:27:08 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,17 +1,11 @@
-
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  console.error('Supabase URL e Anon Key são obrigatórios');
-}
-
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     persistSession: true,
     storage: localStorage,
-    autoRefreshToken: true,
   }
 });
